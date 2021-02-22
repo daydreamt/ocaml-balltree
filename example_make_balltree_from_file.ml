86ad5976c04dd1edbbe7df61f3dd19019dc8d776
daydreamt <github@moraitak.is>
2021-02-22 21:28:34 +0100

    Creating balltrees from the big sentence embeddings now works!

--- example_make_balltree_from_file.ml
+++ example_make_balltree_from_file.ml
@@ -1,14 +1,10 @@
 #use "balltree.ml"
 
-let embeddings_path = "good_big_sentence_embeddings.txt" ;;
+let embeddings_path = "good_big_sentence_embeddings.txt" in
 
 (* In_channel.read_lines embeddings_path |> List.map ~f:(fun x -> (String.split ~on:'\t' x) |> (Array.of_list_map ~f:Float.of_string) |> Tensor.of_float1) ;; *)
 
-let t1 = In_channel.read_lines embeddings_path |> Array.of_list_map ~f:(fun x -> (String.split ~on:'\t' x) |> (Array.of_list_map ~f:Float.of_string)) |> Tensor.of_float2;;
+let t1 = In_channel.read_lines embeddings_path |> Array.of_list_map ~f:(fun x -> (String.split ~on:'\t' x) |> (Array.of_list_map ~f:Float.of_string)) |> Tensor.of_float2 in
 let bt1 = construct_balltree t1;;
 
-
-
-
-
-
+Stdio.printf "GB used: %f" (Gc.allocated_bytes () /. (1024. *. 1024. *. 1024.));;
