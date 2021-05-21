a1d89cd6f95dc2b8aeca0ddfd6e1309e96169e7b
daydreamt <github@moraitak.is>
2021-05-21 22:29:30 +0200

    Pass embedding files to as arguments

--- examples/baseline_make_balltree_from_file.py
+++ examples/baseline_make_balltree_from_file.py
@@ -1,15 +1,21 @@
+import argparse
+import resource
+import sys
 import numpy as np
-from scipy.sparse import csr_matrix
+#from scipy.sparse import csr_matrix
 from sklearn.neighbors import BallTree
 
+parser = argparse.ArgumentParser(usage="")
+parser.add_argument('--embeddings', help="e.g. --embeddings good_big_sentence_embeddings.txt", required=True)
+args = parser.parse_args()
+
 embeddings = []
-with open("good_big_sentence_embeddings.txt") as f:
+with open(args.embeddings) as f:
     for line in f.readlines():
         x = np.array(line.strip().split())
         embeddings.append(x.astype(np.float))
-        
+
 embeddings = np.concatenate(embeddings)
-
-
 tree = BallTree(embeddings,
                 leaf_size=embeddings.shape[0])
+print("GB used: {}".format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / (1024**3)))
--- examples/example_make_balltree_from_file.ml
+++ examples/example_make_balltree_from_file.ml
@@ -1,6 +1,13 @@
-#use "balltree.ml"
+open Core
+open Torch
+open Balltree
 
-let embeddings_path = "good_big_sentence_embeddings.txt"
-let t1 = In_channel.read_lines embeddings_path |> Array.of_list_map ~f:(fun x -> (String.split ~on:'\t' x) |> (Array.of_list_map ~f:Float.of_string)) |> Tensor.of_float2
-let bt1 = construct_balltree t1
-Stdio.printf "GB used: %f" (Gc.allocated_bytes () /. (1024. *. 1024. *. 1024.))
+let () =
+
+  let cmd_args = Sys.get_argv () in
+  if Array.length cmd_args <> 2
+  then Printf.failwithf "usage: %s good_big_sentence_embeddings.txt" cmd_args.(0) ();
+  let embeddings_path = cmd_args.(1) in
+  let t1 = In_channel.read_lines embeddings_path |> Array.of_list_map ~f:(fun x -> (String.split ~on:'\t' x) |> (Array.of_list_map ~f:Float.of_string)) |> Tensor.of_float2 in
+  let _ = Balltree.construct_balltree t1 in
+  Stdio.printf "GB used: %f\n" (Gc.allocated_bytes () /. (1024. *. 1024. *. 1024.))
